67ad9d1c7816053bd954f62a4af81963de353d72
Saveleva Kristina <savelevakristina1995@yandex.ru>
2022-04-06 01:40:48 +0300

    Update 2022-04-05T22:40:47.645Z

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ $('.slider-map').slick({
     focusOnSelect: true
 })
 
-$('.holder__slider').slick({
+$('.holder__slider, .shop__slider').slick({
     infinite: true,
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>', 
@@ -77,5 +77,9 @@ $('.quantity-button').on('click', function(){
 
 let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() -1) * $('.summ').data('guests');
     $('.summ').html('$' + summ);
+
+    $('.surfboard-box__circle').on('click', function(){
+        $(this).toggleClass('active')
+    });
 
 });
